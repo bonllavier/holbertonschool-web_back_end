eed97bd31af06dd84893da5dd9bf32c01da7579d
javb92 <mentor37_@hotmail.com>
2020-05-04 23:18:21 -0500

    completed task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module 6"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """"sum of diferent types of data"""
+    return sum(mxd_lst)
