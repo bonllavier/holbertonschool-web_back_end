92d20a4d6eef656b2437590b47085de319499d98
javb92 <mentor37_@hotmail.com>
2020-05-05 19:41:28 -0500

    completed task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,12 +2,12 @@
 """module 4"""
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """wait_n"""
     list1 = []
     for x in range(n):
-        list1.append(await wait_random(max_delay))
+        list1.append(await task_wait_random(max_delay))
     return sorted(list1)
