770acb25fd81d5954e1862f8aa6eb53727e0a6a2
javb92 <mentor37_@hotmail.com>
2020-05-06 15:45:52 -0500

    first commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""module 0"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """Yield numbers from 0 to `to` every `delay` seconds."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
