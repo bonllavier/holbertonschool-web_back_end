455f93702885e53a07be6025b1e141a6c6f05cf6
javb92 <mentor37_@hotmail.com>
2020-05-06 17:44:20 -0500

    fix task 1 - added return annotations

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """module 1"""
 import random
 import asyncio
-from typing import Generator
+from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """async_comprehension"""
     result = [i async for i in async_generator()]
     return result
