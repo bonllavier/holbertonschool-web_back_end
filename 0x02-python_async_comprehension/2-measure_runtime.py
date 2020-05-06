368573cf10e4119f35e1d36c1ebf4109f64fdcae
javb92 <mentor37_@hotmail.com>
2020-05-06 18:03:11 -0500

    completed task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""module 2"""
+import asyncio
+from typing import Generator, List
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure_runtime"""
+    ini = time.perf_counter()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
+    elapsed = time.perf_counter() - ini
+    return elapsed
