6b792caddcdbbb73bc89e32656329c1b0b0c60ad
javb92 <mentor37_@hotmail.com>
2020-05-06 18:10:57 -0500

    fixed gather declaration task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,9 +9,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measure_runtime"""
     ini = time.perf_counter()
-    await asyncio.gather(async_comprehension(),
-                         async_comprehension(),
-                         async_comprehension(),
-                         async_comprehension())
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
     elapsed = time.perf_counter() - ini
     return elapsed
