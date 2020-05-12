0bb914414293254e03a807f3a796617cb8b5fb3b
javb92 <mentor37_@hotmail.com>
2020-05-11 23:55:16 -0500

    changed permissions

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -4,15 +4,18 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache"""
+    """BasicCache
+    """
 
     def put(self, key, item):
-        """put"""
+        """put
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """get """
+        """get
+        """
         if key in self.cache_data:
             return self.cache_data[key]
         return None
